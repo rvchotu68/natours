84e8b7e9895cb65abefdea75c3300e06684f961b
Aravind Kumar <arvind.arvindkumar1994@gmail.com>
2023-01-15 13:50:09 +0530

    Modifed the import script to import users and reviews as well.

--- dev-data/data/import-data.js
+++ dev-data/data/import-data.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 
 const Tour = require('../../models/tour.model');
+const User = require('../../models/users.model');
+const Review = require('../../models/reviews.model');
 
 dotenv.config({ path: `./../../ignore/config.env` });
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 console.log(process.env.DATABASE_CLOUD);
 const DB_URI = process.env.DATABASE_CLOUD.replace(
@@ -25,6 +31,10 @@ mongoose
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, {
+      validateBeforeSave: false,
+    });
+    await Review.create(reviews);
     console.log('data imported');
   } catch (err) {
     console.log('data import failed with error:', err);
@@ -35,6 +45,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany({});
+    await User.deleteMany({});
+    await Review.deleteMany({});
     console.log('data deleted');
   } catch (err) {
     console.log('data deletion failed with error:', err);
